7e71ff41d22a7b5d714f1968ff7c24b3b9adb4c0
samuel law <samuel.law2@gmail.com>
2020-03-04 07:52:37 -0600

    hopefully it will ignore

--- CNC_TOOLBOX/gui/mainwindow.py
+++ CNC_TOOLBOX/gui/mainwindow.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'C:\Users\Samuel\Documents\CodingProjects\Python\in_progress\CNC_TOOLBOX\CNC_TOOLBOX\gui\mainwindow.ui',
 # licensing of 'C:\Users\Samuel\Documents\CodingProjects\Python\in_progress\CNC_TOOLBOX\CNC_TOOLBOX\gui\mainwindow.ui' applies.
 #
-# Created: Wed Mar  4 07:28:10 2020
+# Created: Wed Mar  4 07:51:47 2020
 #      by: pyside2-uic  running on PySide2 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
