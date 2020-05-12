c062e2671b08fcb3cb3bd550d9e604f40d921929
samuel law <samuel.law2@gmail.com>
2020-05-12 15:14:17 -0500

    adds auto venv creation and package installation

--- CNC_TOOLBOX/setup.py
+++ CNC_TOOLBOX/setup.py
@@ -15,7 +15,6 @@ sudo code
 
 import sys
 import os
-import site
 import platform
 import subprocess
 import shutil
@@ -44,18 +43,21 @@ def linux_extra_sauce():
 
 def install_packages():
     """
-    install gscrape.py as global. It's the heart of CNC toolbox that allows
+    "install" (really just copy) gscrape.py. It's the heart of CNC toolbox that allows
     text to be turned into organized lists.
     """
 
-    if site.ENABLE_USER_SITE:
-        dst = site.USER_SITE
-        cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
-        src = os.path.join(cwd, 'packages/gscrape.py')
-        try:
-            shutil.copy(src, dst)
-        except Exception as e:
-            print(e)
+    print('installing gscrape...')
+    this_dir = str(os.path.dirname(os.path.realpath(sys.argv[0])))
+    # dst = './.venv/Lib/site-packages'
+    dst = os.path.join(this_dir, '.venv', 'Lib', 'site-packages')
+    src = os.path.join(this_dir, 'packages', 'gscrape.py')
+    try:
+        shutil.copy(src, dst)
+    except Exception as e:
+        print(e)
+    else:
+        print('gscrape installed successfully!')
 
 
 def create_venv():
@@ -64,6 +66,14 @@ def create_venv():
     installed on
     """
 
+    this_dir = str(os.path.dirname(os.path.realpath(sys.argv[0])))
+    cmd = ['python', '-m', 'venv', '.venv']
+    return_status = subprocess.run(cmd, cwd=this_dir)
+    if return_status:
+        print('.venv already exists...')
+    elif not return_status:
+        print('virtual enviroment created!')
+
 
 def main():
     """
@@ -71,27 +81,17 @@ def main():
     """
 
     if int(sys.version[0]) == 3:  # make sure python3 is being used
-        # put in dependencies
-        dependencies = [
-            'setuptools',
-            'PySide2==5.14.0',
-            'pyperclip',
-            'tendo',
-            'matplotlib'
-        ]
-        for d in dependencies:
-            try:  # if not met install it
-                p = subprocess.Popen(
-                    [sys.executable, "-m", "pip3", "install", str(d)],
-                    stdout=subprocess.PIPE
-                )
-                output = p.communicate()[0].decode('ASCII')
-                if p.returncode:
-                    print(output)
-                else:
-                    print(output)
-            except Exception as e:
-                print(e)
+        # create virtual enviroment
+        print('creating virtual enviroment...')
+        create_venv()
+
+
+        # install in dependencies
+        print('installing required packages...')
+        this_dir = str(os.path.dirname(os.path.realpath(sys.argv[0])))
+        cmd = ['./.venv/Scripts/pip3.exe', 'install', '-r', 'requirements.txt']
+        return_status = subprocess.run(cmd, cwd=this_dir)
+        print('packages installed with return stats:', return_status)
 
         # perform linux magic
         if platform.system() == 'Linux':
